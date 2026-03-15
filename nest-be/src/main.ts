590164ca1f0f72ceadf2d52866aa6052eb71473b
Istvan Sandor <steven.ford.j@gmail.com>
2026-03-15 22:01:16 +0000

    fix: helm template 10

--- nest-be/src/main.ts
+++ nest-be/src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   }
 
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  console.log('Listening on port ' + (process.env.PORT || 3000));
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
